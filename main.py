bd69858e0946f84d30da96ebd88a880358237d58
davex-ai <omotoyedave4@gmail.com>
2026-01-10 17:11:14 +0100

    connect

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
+from database import engine
+
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"status": "Food API running"}
+
+
+@app.get("/db-test") #i guess this is for definning route
+def db_test():
+    try:
+        with engine.connect() as conn:
+            return {"db": "connected"}
+    except Exception as e:
+        return {"error": str(e)} # what is this?
