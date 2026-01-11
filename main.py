c4406d8b8fc01743b36d56a972c94d2f46c14b25
davex-ai <omotoyedave4@gmail.com>
2026-01-11 20:50:55 +0100

    adding original price to model. remove loud logs

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 from fastapi import FastAPI
 from sqlalchemy import text
-
 from db import engine, Base
-from models import Base
-import os
-import db
 
 app = FastAPI()
 
+# Create tables ONCE (okay for learning, later use migrations)
 Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
     return {"status": "Food API running"}
 
-
 @app.get("/db-test")
 def db_test():
-    print("ENGINE URL INSIDE ROUTE =", engine.url)
-
-    try:
-        with engine.connect() as conn:
-            result = conn.execute(text("SELECT 1")).all()
-            result_list = [row[0] for row in result]
-            return {"result" : result_list}
-    except Exception as e:
-        return {"error": str(e)}
+    with engine.connect() as conn:
+        conn.execute(text("SELECT 1"))
+    return {"db": "connected"}
