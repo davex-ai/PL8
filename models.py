6c5cfbf37dcb384d0ddb41840395cffffdeb0e23
davex-ai <omotoyedave4@gmail.com>
2026-01-11 18:41:44 +0100

    models.py

--- models.py
+++ models.py
@@ -1,8 +1,5 @@
-from sqlalchemy import Column, Integer, String
+# models.py
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
 from db import Base
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
+from datetime import datetime
