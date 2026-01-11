83c53e8c66a23e61e12676f777ce7020ac49b2bc
davex-ai <omotoyedave4@gmail.com>
2026-01-11 18:43:32 +0100

    main.py Product

--- models.py
+++ models.py
@@ -13,3 +13,19 @@ class Category(Base):
     products = relationship("Product", back_populates="category")
 
 
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    image = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    stock = Column(Integer, default=0)
+    rating = Column(Float, default=0.0)
+    num_reviews = Column(Integer, default=0)
+    category_id = Column(Integer, ForeignKey("categories.id"))
+
+    category = relationship("Category", back_populates="products")
+    reviews = relationship("Review", back_populates="product")
+
+
