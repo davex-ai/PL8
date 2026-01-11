c134f797136842b93f6573e70a7587aed0cd6a8c
davex-ai <omotoyedave4@gmail.com>
2026-01-11 17:54:16 +0100

    debug: changed port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from sqlalchemy import text
+from sqlalchemy.engine import row
 
 from db import engine, Base
 import os
@@ -23,6 +24,8 @@ def db_test():
 
     try:
         with engine.connect() as conn:
-            return {"result": conn.execute(text("SELECT 1")).fetchall()}
+            result = conn.execute(text("SELECT 1")).all()
+            result_list = [row[0] for row in result]
+            return {"result" : result_list}
     except Exception as e:
         return {"error": str(e)}
