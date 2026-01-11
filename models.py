e793d88ce1825ba038ff999e3ca8d07a8615429a
davex-ai <omotoyedave4@gmail.com>
2026-01-11 18:42:58 +0100

    main.py Category

--- models.py
+++ models.py
@@ -3,3 +3,13 @@ from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from db import Base
 from datetime import datetime
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+
+    products = relationship("Product", back_populates="category")
+
+
